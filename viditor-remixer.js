bf52db532e95325971c00dbe455c618ac9e1e06f
Andrew McPherson <andrewmcp333@gmail.com>
2014-11-22 06:23:26 -0500

    Embedded a video through HTML5.

--- viditor-remixer.js
+++ viditor-remixer.js
@@ -2,7 +2,11 @@ if(Meteor.isClient)
 {
 	Meteor.startup(function()
 	{
-		console.log("Hello Client!");
+		var video_id = "01";
+		$("video").find("source#mp4").attr("src", "video." + video_id + ".mp4");
+		$("video").find("source#webm").attr("src", "video." + video_id + ".webm");
+		$("video").find("source#ogv").attr("src", "video." + video_id + ".ogv");
+		$("video").get(0).load();
 	});
 }
 
