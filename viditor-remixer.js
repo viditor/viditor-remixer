57cdc9bb5c2b74cc94dee66d9ecffd7df441f847
Andrew McPherson <andrewmcp333@gmail.com>
2014-11-22 07:18:07 -0500

    Defined a system for switching between videos.

--- viditor-remixer.js
+++ viditor-remixer.js
@@ -1,19 +1,46 @@
+Videos = new Meteor.Collection("videos");
+
 if(Meteor.isClient)
 {
 	Meteor.startup(function()
 	{
+		Session.set("filehandle", "01");
+		
 		var video_id = "01";
-		$("video").find("source#mp4").attr("src", "video." + video_id + ".mp4");
-		$("video").find("source#webm").attr("src", "video." + video_id + ".webm");
-		$("video").find("source#ogv").attr("src", "video." + video_id + ".ogv");
-		$("video").get(0).load();
+		
+		Deps.autorun(function()
+		{
+			var filehandle = Session.get("filehandle");
+			
+			$("video").find("source#mp4").attr("src", "video." + filehandle + ".mp4");
+			$("video").find("source#webm").attr("src", "video." + filehandle + ".webm");
+			$("video").find("source#ogv").attr("src", "video." + filehandle + ".ogv");
+			
+			$("video").get(0).load();
+		});
 	});
+	
+	Template.assets.videos = function()
+	{
+		return Videos.find({});
+	}
+	
+	Template.assets.events =
+	{
+		"click .video-asset": function()
+		{
+			var filehandle = this.filehandle;
+			Session.set("filehandle", filehandle);
+		}
+	}
 }
 
 if(Meteor.isServer)
 {
 	Meteor.startup(function()
 	{
-		console.log("Hello Server!");
+		Videos.remove({});
+		Videos.insert({filehandle: "01"});
+		Videos.insert({filehandle: "02"});
 	});
 }
